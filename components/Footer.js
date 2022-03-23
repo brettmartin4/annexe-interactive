400816371e886bd907b5062fa38dc995a08fc514
brettmartin4 <brettmatthewmartin0@gmail.com>
2022-03-22 21:59:55 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Copyright 2015-2022 <img src="/AILOGO.svg" alt="AI Logo" className={styles.logo} /> All Rights Reserved.
       </footer>
     </>
   )
