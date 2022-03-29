e37d305a858887c3895008418a037e1b4fb10a45
Brett Martin <brettmatthewmartin0@gmail.com>
2022-03-29 16:44:10 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Annexe Interactive</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/AILOGO.ico" />
       </Head>
 
       <main>
