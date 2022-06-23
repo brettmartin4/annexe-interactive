249bb5b85d9542c11e3ed2634ffc458ec0f5f4f0
Brett Martin <brettmatthewmartin0@gmail.com>
2022-06-22 23:31:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,26 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container-1">
+    
+      <div className="container">
+      <Head>
+        <title>Annexe Interactive</title>
+        <link rel="icon" href="/AILOGO.ico" />
+      </Head>
 
       <div className="box-1">
         <h3>Links</h3>
-        <p>Link 1\n link 2\n link 3</p>
+        <p>
+          Link 1 <br> 
+          link 2 <br> 
+          link 3
+        </p>
       </div>
     
       <div className="box-2">
         <h3>Annexe Interactive</h3>
         <p>
-          Site currently under construction.
+          Site currently under construction. <br>
           For all business inquiries, please contact us at admin@annexeinteractive.com
         </p>
       </div>
