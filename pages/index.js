5c9c72416a7429e4dfb63d69227471ced18a63fd
Annexe Interactive <annexeinteractive@gmail.com>
2022-08-18 12:12:47 -0400

    Fix height issue

--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,43 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-    <div className="container-1">
-    
-      
-      <Head>
-        <title>Annexe Interactive</title>
-        <link rel="icon" href="/AILOGO.ico" />
-      </Head>
 
-    
-      <div className="box-1">
-        <h3>Links</h3>
-        <p>
-          <Link href="/">Home</Link> <br /> 
-          <Link href="/projects">Projects</Link> <br /> 
-          <Link href="/aboutus">About Us</Link>
-        </p>
+      <div className="container-2">
+        <Header />
       </div>
+
+      <div className="container-1">
+      
+        
+        <Head>
+          <title>Annexe Interactive</title>
+          <link rel="icon" href="/AILOGO.ico" />
+        </Head>
+
+      
+        <div className="box-1">
+          <h3>Links</h3>
+          <p>
+            <Link href="/">Home</Link> <br /> 
+            <Link href="/projects">Projects</Link> <br /> 
+            <Link href="/aboutus">About Us</Link>
+          </p>
+        </div>
+      
+      
+        <div className="box-2">
+          <h3>Annexe Interactive</h3>
+          <p>
+            Site currently under construction. <br />
+            For all business inquiries, please contact us at admin@annexeinteractive.com
+          </p>
+        </div>
     
-    
-      <div className="box-2">
-        <h3>Annexe Interactive</h3>
-        <p>
-          Site currently under construction. <br />
-          For all business inquiries, please contact us at admin@annexeinteractive.com
-        </p>
       </div>
-   
-    </div>
-    
-    <div className="container-2">
-      <Footer />
-    </div>
+      
+      <div className="container-2">
+        <Footer />
+      </div>
     </div>
   )
 }
