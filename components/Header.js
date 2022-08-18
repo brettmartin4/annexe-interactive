9dde0275975df28639c99e45c259c3c4d4957521
Annexe Interactive <annexeinteractive@gmail.com>
2022-08-18 12:47:18 -0400

    Center header

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,11 @@
+import styles from '.Header.module.css'
+
 export default function Header() {
-  return <h1 class="text-center"> Annexe Interactive</h1>
+  return (
+    <>
+      <header className={styles.header}>
+        Annexe Interactive
+      </header>
+    </>
+  )
 }
